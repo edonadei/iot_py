9e2a81aca9f20fe0055f5f095952d2a7e2708421
DONADEI Emrick <emrick.donadei@hotmail.fr>
2019-12-05 16:03:52 +0100

    cleanup

--- 4_movement_detection_red_led.py
+++ 4_movement_detection_red_led.py
@@ -23,4 +23,6 @@ try:
             # When output from motion sensor is LOW
             GPIO.output(GREEN_LED, False)
             GPIO.output(RED_LED, True)
-except KeyboardInterrupt
+except KeyboardInterrupt:
+    p.stop()  # stop the pulse emission
+    GPIO.cleanup()
